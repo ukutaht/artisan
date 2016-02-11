e27671b66bb63b9b620ae9a5a903de0953dedc7a
Uku Taht <uku.taht@gmail.com>
2016-02-11 17:07:01 +0000

    Add stories to board

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule Artisan.PageController do
   use Artisan.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
+  end
+
+  def board(conn, _params) do
+    render(conn, "board.html")
   end
 end
