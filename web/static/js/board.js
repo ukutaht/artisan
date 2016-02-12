a299e8585de2c721cd860f13181190be1990d722
Uku Taht <uku.taht@gmail.com>
2016-02-12 16:51:54 +0000

    Truncate story text

--- web/static/js/board.js
+++ web/static/js/board.js
@@ -1,6 +1,24 @@
 import React from "react"
 import { Link } from 'react-router'
 
+const stories = [
+  {
+    name: "BUG: No hotels found returns 503",
+    number: 1,
+    estimate: 1.75
+  },
+  {
+    name: "Add a ticket type for a non-JCR event",
+    number: 2,
+    estimate: 3.00
+  },
+  {
+    name: "Child ages: make the new designs work",
+    number: 3,
+    estimate: 5.5
+  },
+]
+
 class Board extends React.Component {
   render() {
     return(
@@ -46,41 +64,22 @@ class Board extends React.Component {
             <h3>Ready</h3>
           </div>
           <ul className="stories-list">
-            <li className="stories-list__item">
-              <div className="stories-list__item__title-line">
-                <Link to="javascript://">#513 Add a ticket type for a...</Link>
-                <span className="stories-list__item__estimate">
-                  <i className="ion-connection-bars padded-icon"></i>1.5
-                </span>
-              </div>
-              <div className="stories-list__item__assignee-line">
-                <i className="ion-person"></i>
-              </div>
-            </li>
-
-            <li className="stories-list__item">
-              <div className="stories-list__item__title-line">
-                <Link to="javascript://">#513 Add a ticket type for a...</Link>
-                <span className="stories-list__item__estimate">
-                  <i className="ion-connection-bars padded-icon"></i>1.5
-                </span>
-              </div>
-              <div className="stories-list__item__assignee-line">
-                <i className="ion-person"></i>
-              </div>
-            </li>
-
-            <li className="stories-list__item">
-              <div className="stories-list__item__title-line">
-                <Link to="javascript://">#513 Add a ticket type for a...</Link>
-                <span className="stories-list__item__estimate">
-                  <i className="ion-connection-bars padded-icon"></i>1.5
-                </span>
-              </div>
-              <div className="stories-list__item__assignee-line">
-                <i className="ion-person"></i>
-              </div>
-            </li>
+            { stories.map((story) => {
+              return (<li key={story.number} className="stories-list__item">
+                <div>
+                  <Link to="javascript://" title={story.name} className="truncated-text">
+                    #{story.number} {story.name}
+                  </Link>
+                  <span className="stories-list__item__estimate">
+                    <i className="ion-connection-bars padded-icon"></i>{story.estimate}
+                  </span>
+                </div>
+                <div className="stories-list__item__assignee-line">
+                  <i className="ion-person"></i>
+                </div>
+              </li>
+              )}
+            )}
           </ul>
         </div>
 
