6b7b6ccc93b65a3a1714d9bca980a739da31884e
Uku Taht <uku.taht@gmail.com>
2016-02-15 01:49:38 +0000

    More granular require for react-router

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,7 +1,11 @@
 import "phoenix_html"
 import React from "react"
 import { render } from "react-dom"
-import { IndexRoute, Router, Route, browserHistory } from 'react-router'
+import IndexRoute from 'react-router/lib/IndexRoute'
+import Router from 'react-router/lib/Router'
+import Route from 'react-router/lib/Route'
+import browserHistory from 'react-router/lib/browserHistory'
+
 import Layout from "./layout"
 import Dashboard from "./dashboard"
 import Board from "./board"
--- web/static/js/dashboard.js
+++ web/static/js/dashboard.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from 'react-router'
+import Link from 'react-router/lib/Link'
 
 class Dashboard extends React.Component {
   render() {
