f51d12f5e22c86e7b03f52504ca0faf8c1ce8d25
Uku Taht <uku.taht@gmail.com>
2016-02-18 15:40:59 +0000

    Render json inline in the controller

--- web/stories/controller.ex
+++ web/stories/controller.ex
@@ -4,21 +4,19 @@ defmodule Artisan.Stories.Controller do
   alias Artisan.Stories
 
   def all(conn, _params) do
-    stories = Repo.all(Artisan.Story)
-
-    render(conn, "all.json", %{stories: stories})
+    conn |> json(Repo.all(Artisan.Story))
   end
 
   def by_state(conn, _params) do
-    render(conn, "all.json", %{stories: Stories.by_state()})
+    conn |> json(Stories.by_state())
   end
 
   def create(conn, %{"story" => story_params}) do
     case Stories.create(story_params) do
       {:ok, created} ->
-        conn |> render("one.json", %{story: created})
+        conn |> json(created)
       {:error, changeset} ->
-        conn |> put_status(400) |> render("errors.json", changeset)
+        conn |> invalid(changeset)
     end
   end
 
@@ -27,9 +25,9 @@ defmodule Artisan.Stories.Controller do
 
     case Stories.update(numeric_id, story_params) do
       {:ok, updated} ->
-        conn |> render("one.json", %{story: updated})
+        conn |> json(updated)
       {:error, changeset} ->
-        conn |> put_status(400) |> render("errors.json", changeset)
+        conn |> invalid(changeset)
     end
   end
 
@@ -38,9 +36,13 @@ defmodule Artisan.Stories.Controller do
 
     case Stories.move(numeric_id, state, index) do
       {:ok, updated} ->
-        conn |> render("one.json", %{story: updated})
+        conn |> json(updated)
       {:error, changeset} ->
-        conn |> put_status(400) |> render("errors.json", changeset)
+        conn |> invalid(changeset)
     end
+  end
+
+  defp invalid(conn, %{errors: errors}) do
+    conn |> put_status(400) |> json(%{errors: Enum.into(errors, %{})})
   end
 end
