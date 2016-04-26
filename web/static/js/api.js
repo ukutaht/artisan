cc8b2e711146103002b37c666fa89261bc079edc
Uku Taht <uku.taht@gmail.com>
2016-04-27 00:07:20 +0100

    Redirect user to /login when they're unauthorized

--- web/static/js/api.js
+++ web/static/js/api.js
@@ -1,24 +1,36 @@
 import Request from 'superagent'
 
+const UNAUTHORIZED = 401
+
+function ensureAuthorized(f) {
+  return (err, res) => {
+    if (err && err.status == UNAUTHORIZED) {
+      location.href = "/login"
+    } else {
+      f(err, res)
+    }
+  }
+}
+
 class Api {
   static get(endpoint, callback) {
     return Request.get(endpoint)
     .set('Authorization', `Bearer ${localStorage.getItem('token')}`)
-    .end(callback)
+    .end(ensureAuthorized(callback))
   }
 
   static post(endpoint, payload, callback) {
     return Request.post(endpoint)
     .set('Authorization', `Bearer ${localStorage.getItem('token')}`)
     .send(payload)
-    .end(callback)
+    .end(ensureAuthorized(callback))
   }
 
   static put(endpoint, payload, callback) {
     return Request.put(endpoint)
     .set('Authorization', `Bearer ${localStorage.getItem('token')}`)
     .send(payload)
-    .end(callback)
+    .end(ensureAuthorized(callback))
   }
 }
 
