b4f9d1bb03219029f86892d14d456f09ef5ac5b7
Uku Taht <uku.taht@gmail.com>
2016-07-21 20:59:07 +0100

    Make story cards more responsive

--- app/src/js/stories/card.js
+++ app/src/js/stories/card.js
@@ -11,7 +11,7 @@ class Card extends React.Component {
     if (this.props.story.estimate !== null) {
       return (
        <span className="story-card__estimate">
-         <i className="ion-connection-bars right-padded-icon"></i>
+         <i className="ion-connection-bars right-padded-icon show-desk-and-up"></i>
          {this.props.story.estimate}
        </span>
       )
@@ -30,13 +30,13 @@ class Card extends React.Component {
     return (
       <li className="story-card" data-id={this.props.story.id}>
         <div className="story-card__first-line">
-          <a title={story.name} className="truncated-text clickable" onClick={this.onClick.bind(this)}>
+          <a title={story.name} className="truncated clickable" onClick={this.onClick.bind(this)}>
             {story.number}. {story.name}
           </a>
           {this.displayEstimate()}
         </div>
         <div className="story-card__second-line">
-          <ul className="story-card__tags">
+          <ul className="story-card__tags truncated">
             {tags.map((tag) => {
               return <li className="story-card__tags__item" key={tag}>{tag}</li>
             })}
