95d829d8426ea5c1e471d60c79ef8e8cf485dc00
Uku Taht <uku.taht@gmail.com>
2016-07-25 23:57:07 +0100

    Fix column updating after 'fake moves'

--- app/src/js/column.js
+++ app/src/js/column.js
@@ -5,6 +5,8 @@ import StoryCard from 'stories/card'
 
 let _nextSibling;
 let _ghost;
+let _fromColumn;
+let _toColumn;
 let _dragging = false;
 
 const _sortableInstances = {
@@ -43,15 +45,30 @@ class Column extends React.Component {
       onStart: this.onStart.bind(this),
       onAdd: this.updatePosition.bind(this),
       onUpdate: this.updatePosition.bind(this),
+      onMove: this.onMove.bind(this),
+      onEnd: this.onEnd.bind(this),
       group: 'stories',
-      ghostClass: 'story-card--ghost',
+      ghostClass: 'story-card--ghost'
     });
   }
 
   onStart(evt) {
+    _fromColumn  = this.props.name
     _dragging    = true
     _nextSibling = evt.item.nextElementSibling;
     _ghost = this.refs.sortable.getElementsByClassName('story-card--ghost')[0].cloneNode(true)
+  }
+
+  onMove(evt) {
+    _toColumn = evt.to.dataset.column
+  }
+
+  onEnd(evt) {
+    const sameColumn = _fromColumn === _toColumn
+    const sameIndex = evt.newIndex === undefined || evt.oldIndex === evt.newIndex
+    if (sameColumn && sameIndex) {
+      _dragging = false
+    }
   }
 
   updatePosition(evt) {
