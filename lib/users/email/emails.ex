59a23abb7694fbd61ca4dc0f84d86fbd8f5350a7
Uku Taht <uku.taht@gmail.com>
2016-08-03 18:46:20 +0100

    Extract invite_subject to module var

--- lib/users/email/emails.ex
+++ lib/users/email/emails.ex
@@ -1,13 +1,14 @@
 defmodule Artisan.Users.Email.Emails do
   use Bamboo.Phoenix, view: Artisan.Users.Email.View
   @from "Artisan <noreply@artisan-app.com>"
+  @invite_subject "[Artisan] You've been invited to join Artisan"
 
   def invite(inviter, email, nil) do
     link = "http://localhost:4000/signup?email=#{email}"
 
     base_email
       |> to(email)
-      |> subject("[Artisan] You've been invited to join Artisan")
+      |> subject(@invite_subject)
       |> render(:invite, inviter: inviter, link: link)
   end
 
@@ -16,7 +17,7 @@ defmodule Artisan.Users.Email.Emails do
 
     base_email
       |> to(email)
-      |> subject("[Artisan] You've been invited to join Artisan")
+      |> subject(@invite_subject)
       |> render(:invite_to_project, inviter: inviter, link: link, project: project)
   end
 
