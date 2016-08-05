4c4464843d3a30b170fb1ead907370cacf97f166
Uku Taht <uku.taht@gmail.com>
2016-08-05 13:36:34 +0100

    List phoenix_html in application list to be included in release

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Artisan.Mixfile do
 
   def application do
     [mod: {Artisan, []},
-     applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :slugger,
+     applications: [:phoenix, :phoenix_html, :phoenix_pubsub, :cowboy, :logger, :slugger,
                     :phoenix_ecto, :postgrex, :comeonin, :bamboo]]
   end
 
