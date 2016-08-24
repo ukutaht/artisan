f59a01b42dc40bbd00701e596e54bede67c3d8c8
Uku Taht <uku.taht@gmail.com>
2016-08-24 22:03:43 +0100

    Formatting

--- app/src/js/projects/nav.js
+++ app/src/js/projects/nav.js
@@ -5,33 +5,32 @@ function activeClass(props, tab) {
   return props.activeTab === tab ? 'active' : ''
 }
 
-
 export default function ProjectNav(props) {
   return (
     <nav className="project__nav">
-    <ul className="project__nav__breadcrumb">
-      <li>
-        <Link to={`/${props.project.slug}`} >{props.project.name}</Link>
-      </li>
+      <ul className="project__nav__breadcrumb">
+        <li>
+          <Link to={`/${props.project.slug}`} >{props.project.name}</Link>
+        </li>
 
-      <li>
-        {props.children}
-      </li>
-    </ul>
+        <li>
+          {props.children}
+        </li>
+      </ul>
 
-    <div className="project__nav__tabs">
-      <Link to={`/${props.project.slug}`}
-        className={activeClass(props, 'storyboard')}>
-        <i className="ion-grid right-padded-icon" />
-        Story board
-      </Link>
+      <div className="project__nav__tabs">
+        <Link to={`/${props.project.slug}`}
+          className={activeClass(props, 'storyboard')}>
+          <i className="ion-grid right-padded-icon" />
+          Story board
+        </Link>
 
-      <Link to={`/${props.project.slug}/settings`}
-        className={activeClass(props, 'settings')}>
-        <i className="ion-gear-b right-padded-icon" />
-        Settings
-      </Link>
-    </div>
+        <Link to={`/${props.project.slug}/settings`}
+          className={activeClass(props, 'settings')}>
+          <i className="ion-gear-b right-padded-icon" />
+          Settings
+        </Link>
+      </div>
     </nav>
   )
 }
